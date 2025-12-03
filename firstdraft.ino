eebe82823cbd2f12cb9ba432d96fd1adf68a2af6
Justanewguy21 <you@example.com>
2025-12-03 22:53:53 +0000

    Initial commit: BIP

---
+++ firstdraft.ino
@@ -0,0 +1,14 @@
+//BMP 280 TESTING
+#include <Wifi.h>
+#include <SPI.h> //data transfer between systesm
+#include <SD.h> //microSD
+#include <Adafruit_BMP280.h> //include BMP 280 library to use
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
